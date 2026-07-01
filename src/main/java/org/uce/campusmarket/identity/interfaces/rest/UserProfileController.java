d0aa0c75b25d55acc84b5a82cee3bb9ddc8fff6b
jgsoto <sotoj9268@gmail.com>
2026-06-30 19:58:41 -0500

    refactor: crear endpoint para subir imagenes

--- src/main/java/org/uce/campusmarket/identity/interfaces/rest/UserProfileController.java
+++ src/main/java/org/uce/campusmarket/identity/interfaces/rest/UserProfileController.java
@@ -3,12 +3,17 @@ package org.uce.campusmarket.identity.interfaces.rest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.http.MediaType;
+import org.springframework.web.multipart.MultipartFile;
+import org.uce.campusmarket.identity.application.usecase.UploadProfilePhotoUseCase;
 import org.uce.campusmarket.identity.application.dto.BasicUserResponse;
 import org.uce.campusmarket.identity.application.dto.UserProfileRequest;
 import org.uce.campusmarket.identity.application.dto.UserProfileResponse;
 import org.uce.campusmarket.identity.application.usecase.GetUserProfileUseCase;
 import org.uce.campusmarket.identity.application.usecase.UpdateUserProfileUseCase;
 import org.uce.campusmarket.identity.application.usecase.GetUsersUseCase;
+import org.uce.campusmarket.identity.application.usecase.UploadProfilePhotoUseCase;
+
 
 import java.util.List;
 import java.util.UUID;
@@ -21,6 +26,7 @@ public class UserProfileController {
     private final GetUserProfileUseCase getUserProfileUseCase;
     private final UpdateUserProfileUseCase updateUserProfileUseCase;
     private final GetUsersUseCase getUsersByIdsUseCase;
+    private final UploadProfilePhotoUseCase uploadProfilePhotoUseCase;
 
     @GetMapping("/{id}")
     public ResponseEntity<UserProfileResponse> getProfile(@PathVariable UUID id) {
@@ -35,6 +41,21 @@ public class UserProfileController {
     ) {
         updateUserProfileUseCase.execute(userId, request);
         return ResponseEntity.ok().build();
+    }
+
+    @PostMapping(
+            value = "/photo",
+            consumes = MediaType.MULTIPART_FORM_DATA_VALUE
+    )
+    public ResponseEntity<UserProfileResponse> uploadPhoto(
+            @RequestHeader("X-User-Id") UUID userId,
+            @RequestParam("file") MultipartFile file
+    ) {
+
+        UserProfileResponse response =
+                uploadProfilePhotoUseCase.execute(userId, file);
+
+        return ResponseEntity.ok(response);
     }
 
     @PostMapping("/bulk")
